ba1b099fec7577269074d0f99ba3f9019fbd5117
Johan Segerfeldt <johan.segerfeldt@me.com>
2026-03-26 08:07:12 +0100

    Merge implementation files

--- src/runtime/lanes.c
+++ src/runtime/lanes.c
@@ -3,6 +3,25 @@
 
 #include <stdlib.h>
 
+String* binarylane__toStringRC(uint64_t value, uint32_t length) {
+    if (length == 0 || length > 64) {
+        panic("binarylane__toStringRC expects length in [1, 64]");
+    }
+
+    char* buffer = malloc((size_t) length + 1);
+    if (!buffer) panic("Out of memory in binarylane__toStringRC");
+
+    for (uint32_t i = 0; i < length; i++) {
+        uint64_t bit = 1ULL << (length - 1 - i);
+        buffer[i] = (value & bit) ? '1' : '0';
+    }
+    buffer[length] = '\0';
+
+    String* result = String¸fromCString(buffer);
+    free(buffer);
+    return result;
+}
+
 String* ternarylane__toStringRC(uint64_t x0, uint64_t x1, uint32_t length) {
     if (length == 0 || length > 64) {
         panic("ternarylane__toStringRC expects length in [1, 64]");
